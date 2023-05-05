8cae8f5b417623d2759d452b838faad1c0d8fb0c
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-05-05 19:41:42 +0100

    Add files via upload

---
+++ timeDifference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.Clear();
+        Console.Write("write time start: ");
+        DateTime inicio = DateTime.Parse(Console.ReadLine());
+
+        Console.Write("write time end : ");
+        DateTime fim = DateTime.Parse(Console.ReadLine());
+
+        TimeSpan duracao = fim - inicio;
+        double horas = duracao.TotalHours;
+
+        Console.WriteLine("time difference: " + horas.ToString("F2") + " hours");
+    }
+}
